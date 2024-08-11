9e47b1803a69071915f071de33b217f06cc0f163
adatdt <46507975+adatdt@users.noreply.github.com>
2024-08-11 18:51:27 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -329,7 +329,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '2pgCA3VXdwe47h04z4qUEa6118txGawF';
+$config['encryption_key'] = 'xxxxxxxxxxxxiopouiuxxxxxxxxxxxx';
 
 /*
 |--------------------------------------------------------------------------
